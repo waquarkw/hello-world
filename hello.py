4c0658ac7a8ed9b64720a66d7fc46ee2682cf10c
Khalid Waquar <thisiskhalid76@gmail.com>
2023-01-17 19:04:31 -0500

    First commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+print(‘Hello, World!’)
+
+
+
+
+
+
+Hi my full name is Khalid WAquar. I currently work for a company called El toro IP Targeting. I have former experience as a Risk Analyst with AMazon.com. 
+I have worked there for 2 years. 
+
+
+git add hello.py
